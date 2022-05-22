3cde1ef6ee69f9c89420df5d6846be0ae8931ac2
Andrea Sarai Juárez Munguia <andrea.juarez@ids.com.mx>
2022-05-22 16:09:23 -0500

    implementing restcontroller

--- src/main/java/com/platzi/market/persistence/entity/Categoria.java
+++ src/main/java/com/platzi/market/persistence/entity/Categoria.java
@@ -38,4 +38,10 @@ public class Categoria {
         this.estado = estado;
     }
 
+    public List<Producto> getProductos() {
+        return productos;
+    }
+    public void setProductos(List<Producto> productos) {
+        this.productos = productos;
+    }
 }
